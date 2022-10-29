066563f86a2ce5ae893043a513af2c9a0adb1b32
osama morad <osamamorad85@gmail.com>
2022-10-30 00:25:58 +0200

    add text form field to email and password

---
+++ lib/login_screen.dart
@@ -0,0 +1,66 @@
+
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: double.infinity,
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+
+                  ),
+                  //textAlign: TextAlign.start,
+                ),
+              ),
+              TextFormField(
+                onFieldSubmitted: (value){
+                  print(value);
+                },
+                onChanged: (value){
+                  print(value);
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(
+                    Icons.email,
+                  ),
+                  labelText: 'Email',
+                ),
+              ),
+              TextFormField(
+                onFieldSubmitted: (value){
+                  print(value);
+                },
+                onChanged: (value){
+                  print(value);
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(
+                    Icons.remove_red_eye,
+                  ),
+                  labelText: 'Password',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ) ,
+    );
+  }
+}
